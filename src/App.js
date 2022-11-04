a3191e32f059ef8ca19c23b3d7519387b39b2f3a
Akintibubo Pelumi <pelumitemitope10@gmail.com>
2022-11-04 23:17:03 +0100

    HNG second task

--- src/App.js
+++ src/App.js
@@ -1,82 +1,17 @@
 import React from "react";
+import Home from "./Home";
+import Contact from "./contact";
+import {Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <main>
-      <img id="profile__img" src="./1.jpg" alt="This is me" />
-      <p id="slack">pels_x</p>
-      <p className="name">Akintibubo Pelumi</p>
-      <a
-        href="https://twitter.com/TemitopePels"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button id="twitter">Twitter Link</button>
-      </a>
-
-      <a
-        href="https://training.zuri.team"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button id="btn__zuri">Zuri Team</button>
-      </a>
-      <a
-        href="http://books.zuri.team"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button id="books">Zuri Books</button>
-      </a>
-
-      <a
-        href="https://books.zuri.team/python-for-beginners?ref_id=<pels_X>"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button id="book__python">Python Books</button>
-      </a>
-      <a
-        href="https://background.zuri.team"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button id="pitch">Background Check for Coders</button>
-      </a>
-
-      <a
-        href="https://books.zuri.team/design-rules"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <button id="book__design">Design Books</button>
-      </a>
-      <div id="logo">
-        <img
-          className="image"
-          src="https://www.logo.wine/a/logo/Slack_Technologies/Slack_Technologies-Mark-Logo.wine.svg"
-          alt="slack"
-        />
-        <img
-          className="image"
-          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-          alt="github"
-        />
-      </div>
-      <hr />
-      <div id="footer">
-        <h3 className="zuri">
-          Zuri<span style={{color: "#fc2390" }}>.</span>Internship
-        </h3>
-        <p className="HNG">HNG Internship 9 Frontend Task</p>
-        <img
-          className="footer"
-          src="https://www.vanguardngr.com/wp-content/uploads/2020/11/INGRESS.jpg"
-          alt="ingressive for good"
-        />
-      </div>
-    </main>
-  );
+function App (){
+  return(
+    <section>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </section>
+  )
 }
 
 export default App;
---
+++ src/contact.js
@@ -0,0 +1,52 @@
+import React from "react";
+function Contact() {
+  return (
+    <><div id="main">
+      <h1 id="title">Contact Me</h1>
+      <p>Hi there, contact me to ask me about anything you have in mind</p>
+      <form>
+        <div className="side">
+          <div>
+            <label for="first_name">First Name</label>
+            <input
+              type="text"
+              id="first_name"
+              placeholder="Enter your firstname" />
+          </div>
+
+          <div>
+            <label for="last_name">Last Name</label>
+            <input
+              type="text"
+              id="last_name"
+              placeholder="Enter your lastname" />
+          </div>
+        </div>
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="yourname@email.com" />
+        <label for="comment">Message</label>
+        <textarea
+          id="message"
+          name="comment"
+          placeholder="Send me a message and i will reply you as soon as possible" />
+        <input type="radio" id="agree" />
+        <label for="agree">
+          You agree to provide your data to Pelumi who may contact you
+        </label>
+        <input type="submit" id="btn__submit" value="Send Message" />
+      </form>
+    </div>
+    <hr />
+    <div id="footer">
+        <h3 className="zuri">
+          Zuri<span style={{ color: "#fc2390" }}>.</span>Internship
+        </h3>
+        <p className="HNG">HNG Internship 9 Frontend Task</p>
+        <img
+          className="footer"
+          src="https://www.vanguardngr.com/wp-content/uploads/2020/11/INGRESS.jpg"
+          alt="ingressive for good" />
+      </div></>
+  );
+}
+export default Contact;
